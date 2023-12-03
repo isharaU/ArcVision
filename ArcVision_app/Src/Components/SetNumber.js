05a2edc8131bcb69eccd48683b512256928d81f1
isharaU <isharauditha5@gmail.com>
2023-12-03 10:22:52 +0530

    roll back

--- ArcVision_app/Src/Components/SetNumber.js
+++
@@ -1,6 +0,0 @@
-import { sendLocationSMS } from "../Components/SendSms";
-
-export const setNumberFunction = (phoneNumber) => {
-  console.log("Setting phone number:", phoneNumber);
-  sendLocationSMS(phoneNumber);
-};
